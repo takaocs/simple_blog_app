6488e2d6afbcbbb942f96177fe8786757c2c75ed
takaocs <takaob0718@gmail.com>
2020-03-22 02:06:08 +0000

    一覧ページ（検索機能などはなし）の作成

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+
+from django.views.generic import ListView
+
+# Models
+from .models import Article
 
 # Create your views here.
+
+
+class ArticleListView(ListView):
+    """
+    一覧ページ
+    """
+    # クエリセット
+    queryset = Article.objects.filter(is_deleted=False)
+    # テンプレート
+    template_name = 'blog/article_list.html'
