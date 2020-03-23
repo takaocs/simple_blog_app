93c873e8256f2f35ec8c043ea3b4995c09ad6850
takaocs <takaob0718@gmail.com>
2020-03-23 08:28:55 +0000

    記事を公開する場合、タイトルと内容を必須とするバリデーションを追加

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -18,3 +18,24 @@ class ArticlePostForm(forms.ModelForm):
         widgets = {
             'contents': SummernoteWidget()
         }
+
+    def _add_error_required(self, field, msg):
+        """
+        必須のエラーを追加する
+        """
+        v = self.cleaned_data.get(field)
+        if not v:
+            # 値が取得できなかった場合はエラーを追加する
+            self.add_error(field, msg)
+
+    def clean_is_open(self):
+        """
+        公開の場合、タイトル・内容は必須とする
+        """
+        is_open = self.cleaned_data.get('is_open', False)
+        if is_open:
+            # 公開の場合はエラーチェック
+            self._add_error_required('title', 'タイトルを入力してください。')
+            self._add_error_required('contents', '記事の内容を入力してください。')
+
+        return is_open
