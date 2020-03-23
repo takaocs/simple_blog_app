2371cf7ff79c8c8249ebab0d34fbdf57d893ccce
takaocs <takaob0718@gmail.com>
2020-03-23 03:04:18 +0000

    記事投稿のテストコードを作成（RED）

--- apps/blog/tests/test_forms.py
+++ apps/blog/tests/test_forms.py
@@ -11,9 +11,67 @@ from ..forms import ArticlePostForm
 class ArticlePostFormTestCase(TestCase):
     """
     ブログの記事を投稿するフォームのテスト
+
+    - 一時保存（is_open が False）の場合
+        - タイトルが未入力でも登録できるかどうか
+        - 内容が未入力でも登録できるかどうか
+    - 公開（is_open が True）の場合
+        - タイトルが未入力だとエラーになるかどうか
+        - 内容が未入力だとエラーになるかどうか
+        - 公開日時に現在時刻が登録されるかどうか
     """
-    def test_article_temporarily_saved(self):
+    def test_is_open_false_title_not_required(self):
         """
-        一時保存の状態で登録
+        一時保存の場合、タイトルが未入力でも登録できるかどうか
         """
-        ...
+        form = ArticlePostForm(
+            data={
+                'contents': 'テスト',
+                'is_open': False
+            }
+        )
+        self.assertTrue(form.is_valid())
+
+    def test_is_open_false_contents_not_required(self):
+        """
+        一時保存の場合、内容が未入力でも登録できるかどうか
+        """
+        form = ArticlePostForm(
+            data={
+                'title': 'テスト',
+                'is_open': False
+            }
+        )
+        self.assertTrue(form.is_valid())
+
+    def test_is_open_true_title_required(self):
+        """
+        公開の場合、タイトルが未入力の場合エラーとなる
+        """
+        form = ArticlePostForm(
+            data={
+                'contents': 'テスト',
+                'is_open': True
+            }
+        )
+
+        # バリデーションの確認
+        self.assertFalse(form.is_valid())
+        # エラーメッセージも確認
+        elf.assertEqual(form.errors['title'][0], 'タイトルを入力してください。')
+
+    def test_is_open_true_contents_required(self):
+        """
+        公開の場合、内容が未入力の場合エラーとなる
+        """
+        form = ArticlePostForm(
+            data={
+                'title': 'テスト',
+                'is_open': True
+            }
+        )
+
+        # バリデーションの確認
+        self.assertFalse(form.is_valid())
+        # エラーメッセージも確認
+        elf.assertEqual(form.errors['contents'][0], '記事の内容を入力してください。')
