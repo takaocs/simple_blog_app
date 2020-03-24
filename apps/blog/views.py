86361bece93bbc3ffb5653efce433875d94a45a3
takaocs <takaob0718@gmail.com>
2020-03-24 01:45:59 +0000

    記事の新規登録のビュークラスを作成

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 
-# Models
+# Apps
 from .models import Article
+from .forms import ArticlePostForm
 
 # Create your views here.
 
@@ -16,3 +18,15 @@ class ArticleListView(ListView):
     queryset = Article.objects.filter(is_deleted=False)
     # テンプレート
     template_name = 'blog/article_list.html'
+
+
+class ArticleCreateView(CreateView):
+    """
+    新規作成
+    """
+    # フォームクラス
+    form_class = ArticlePostForm
+    # 登録後のリダイレクト先
+    success_url = reverse_lazy('blog:article_list')
+    # テンプレート
+    template_name = 'blog/article_create.html'
